e867ed9794fe74c2258f5f26cc48268f2342fbb6
elias <eliashabibhamidabdu.gmail.com>
2026-04-11 17:25:18 +0300

    G

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SocialLinks from './components/SocialLinks';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import SayHello from './pages/SayHello';
 import Work from './pages/Work';
@@ -8,6 +9,7 @@ import Work from './pages/Work';
 function App() {
   return (
     <div className="w-full min-h-screen bg-obsidian text-ivory font-sans">
+      <ScrollToTop />
       <Navbar />
       <SocialLinks />
       <Routes>
